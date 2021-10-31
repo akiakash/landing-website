fb910f86615e3761464fc062c5c1415302d1ff4a
akiakash <it20072742@my.sliit.lk>
2021-10-31 11:44:35 +0530

    navbar-done

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,29 +1,29 @@
 import React from 'react'
 import{SidebarContainer,Icon,CloseIcon,SidebarWrapper,SidebarMenu,SidebarLink,SidebarRoute,SideBtnWrap} from './SidebarElements'
 
-const Sidebar = () => {
+const Sidebar = ({isOpen,toggle}) => {
     return (
-        <SidebarContainer>
-            <Icon>
+        <SidebarContainer isOpen={isOpen} onClick={toggle}>
+            <Icon onClick={toggle}>
                 <CloseIcon />
             </Icon>  
             <SidebarWrapper>
                 <SidebarMenu>
-                    <SidebarLink to='about'>
+                    <SidebarLink to='about'onClick={toggle}>
                         About
                     </SidebarLink>
-                    <SidebarLink to='discover'>
+                    <SidebarLink to='discover' onClick={toggle}>
                         Discover
                     </SidebarLink>
-                    <SidebarLink to='services'>
+                    <SidebarLink to='services' onClick={toggle}>
                         Services
                     </SidebarLink>
-                    <SidebarLink to='signup'>
+                    <SidebarLink to='signup' onClick={toggle}>
                         Side Up
                     </SidebarLink>
                     </SidebarMenu>
                     <SideBtnWrap>
-                        <SidebarRoute to='/'>
+                        <SidebarRoute to='/' onClick={toggle}>
                             Sign in
                         </SidebarRoute>
                     </SideBtnWrap>
